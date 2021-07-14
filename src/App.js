a6d9dea6d221ad8df8a5eaa7b4fecb31b8e7dbe5
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2021-07-14 20:38:07 -0300

    CSS resetado e ajustado. Criado estrutura do input e o mecanismo de entrada de tarefas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-const App = () => <h1>App</h1>;
+const App = () => {
+  const [value, setValue] = useState("");
 
+  const erase = () => {
+    setValue("");
+  };
+
+  const submit = () => {
+    console.log("submit", value);
+    erase();
+  };
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      submit();
+    } else if (event.key === "Escape") {
+      erase();
+    }
+  };
+
+  return (
+    <section id="app" className="container">
+      <header>
+        <h1 className="title">todo</h1>
+      </header>
+      <section className="main">
+        <input
+          className="new-todo"
+          placeholder="o que precisa ser feito?"
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+      </section>
+    </section>
+  );
+};
 export default App;
